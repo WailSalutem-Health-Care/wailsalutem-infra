438ade25190e11b551d4c35bad200771887cd1e0
Muhammad Faizan <mf_2003@outlook.com>
2025-12-29 14:52:50 +0100

    Fix UUID values in organization seeding scripts for consistency

--- postgres/init/03_create_org_1.sql
+++ postgres/init/03_create_org_1.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS org_alpha;
 
 INSERT INTO wailsalutem.organizations (id, name, schema_name, contact_email)
 VALUES (
-    '00000650-32535-0000-8858-0004560001'::UUID,
+    '00000000-0000-0000-0000-000000000001'::UUID,
     'Organisation Alpha',
     'org_alpha',
     'admin@org-alpha.com'
--- postgres/init/04_create_org_2.sql
+++ postgres/init/04_create_org_2.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS org_beta;
 
 INSERT INTO wailsalutem.organizations (id, name, schema_name, contact_email)
 VALUES (
-    '000987000-0000-6666-8675-00034000002'::UUID,
+    '00000000-0000-0000-0000-000000000002'::UUID,
     'Organisation Beta',
     'org_beta',
     'admin@org-beta.com'
